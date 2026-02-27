455276d6f57d87d4d76fc06a7ffe350b58bd560f
Christoph Hilchenbach <11504129+hilch@users.noreply.github.com>
2026-02-27 12:48:31 +0100

    Add tooltip handler

--- pvi/plcwatch_modules/object_treeview.py
+++ pvi/plcwatch_modules/object_treeview.py
@@ -9,9 +9,45 @@ from pvi import Connection, PviObject, Line, Device, Cpu, Task, Variable
 from pvi.plcwatch_modules.resources import image_files
 
 
+class TreeViewTooltip:
+    def __init__(self, treeview):
+        self.treeview = treeview
+        self.tip_window = None
+        self.item_tooltips = {}
+        self.treeview.bind("<Motion>", self.on_motion)
+        self.treeview.bind("<Leave>", self.hide_tooltip)
+
+    def set_tooltip(self, item_id, tooltip_text):
+        self.item_tooltips[item_id] = tooltip_text
+
+    def on_motion(self, event):
+        item = self.treeview.identify('item', event.x, event.y)
+        if item and item in self.item_tooltips:
+            self.show_tooltip(event, self.item_tooltips[item])
+        else:
+            self.hide_tooltip()
+
+    def show_tooltip(self, event, text):
+        if self.tip_window:
+            self.tip_window.destroy()
+        self.tip_window = tk.Toplevel(self.treeview)
+        self.tip_window.wm_overrideredirect(True)
+        self.tip_window.wm_geometry(f"+{event.x_root + 10}+{event.y_root + 10}")
+        label = tk.Label(self.tip_window, text=text, background="lightyellow",
+                        relief=tk.SOLID, borderwidth=1)
+        label.pack(ipadx=3, ipady=3)
+
+    def hide_tooltip(self, event=None):
+        if self.tip_window:
+            self.tip_window.destroy()
+            self.tip_window = None
+
+
+
 class ObjectTreeView(ttk.Treeview):
     def __init__(self, parent : tk.Tk, pvi_connection : Connection ):
         super().__init__( parent, columns=('name', 'value'), selectmode='browse' )
+        self.tooltip_handler = TreeViewTooltip(self)
         self.pvi_connection = pvi_connection        
         self.image_storage = { str(k) : tk.PhotoImage( file = v ) for k,v in image_files.items() }        
         
@@ -61,7 +97,8 @@ class ObjectTreeView(ttk.Treeview):
                 # insert array variable itself
                 self.insert( struct.name, index = 'end', 
                                 iid = element.name, text = element_name,
-                            image = self.image_storage['array'] )    
+                            image = self.image_storage['array'] ) 
+                self.tooltip_handler.set_tooltip(struct.name, struct.name)   
                 # insert array elements
                 self.expandArray( task, element )
                 #array_variable.kill()                
@@ -70,12 +107,14 @@ class ObjectTreeView(ttk.Treeview):
                 # insert the substructure itself
                 self.insert( struct.name, index = 'end', 
                                          iid = element.name, text = element_name,
-                            image = self.image_storage['struct'] )    
+                            image = self.image_storage['struct'] ) 
+                self.tooltip_handler.set_tooltip(struct.name, struct.name)   
                 self.expandStruct(task, element)           
                 #substructure.kill()
             else:       
                 self.insert( struct.name, index = 'end', iid = element.name, text = element_name,
-                                image = icon, values = (element.value,) )    
+                                image = icon, values = (element.value,) )  
+                self.tooltip_handler.set_tooltip(struct.name, struct.name)  
             #element.kill()
         
         
@@ -106,13 +145,14 @@ class ObjectTreeView(ttk.Treeview):
                         self.insert( array.name, index = 'end', 
                             iid = f'{array.name}[{i1},{i2}]', text = f'[{i1},{i2}]',
                             image = self.image_storage['struct'] )
+                        self.tooltip_handler.set_tooltip(array.name, array.name)
                         struct = Variable( task, f'{array.objectName}[{i1},{i2}]' )
                         self.expandStruct( task, struct )
                     else:
                         self.insert( array.name, index = 'end', 
                                 iid = f'{array.name}[{i1},{i2}]', text = f'[{i1},{i2}]',
                                 image = icon, values = vk )
-
+                        self.tooltip_handler.set_tooltip(array.name, array.name)
             else: # vector
                 i1 = indices[0][0] + j
                 if isinstance( v, OrderedDict):
@@ -120,13 +160,14 @@ class ObjectTreeView(ttk.Treeview):
                     self.insert( array.name, index = 'end', 
                         iid = f'{array.name}[{i1}]', text = f'[{i1}]',
                         image = self.image_storage['struct'] )
+                    self.tooltip_handler.set_tooltip(array.name, array.name)
                     struct = Variable( task, f'{array.objectName}[{i1}]' )
                     self.expandStruct( task, struct )
                 else:
                     self.insert( array.name, index = 'end', 
                             iid = f'{array.name}[{i1}]', text = f'[{i1}]',
                             image = icon, values = v )
-        
+                    self.tooltip_handler.set_tooltip(array.name, array.name)
         
         
     def onTaskClicked( self, item : str, task : Task ):
@@ -145,17 +186,19 @@ class ObjectTreeView(ttk.Treeview):
                     # insert array itself
                     self.insert( item, index = 'end', iid = variable.name, text = name,
                                 image = self.image_storage['array'] )
+                    self.tooltip_handler.set_tooltip(variable.name, variable.name)
                     self.expandArray(task,variable)                
                 elif isinstance(value, OrderedDict): # is variable a struct ?
                     # insert struct itself
                     self.insert( item, index = 'end', iid = variable.name, text = name,
                                 image = self.image_storage['struct'] )    
+                    self.tooltip_handler.set_tooltip(variable.name, variable.name)
                     # insert children
                     self.expandStruct(task, variable)
                 else: # variable with basic datatype        
                     self.insert( item, index = 'end', iid = variable.name, text = name,
                                 image = icon, values = (value,) )    
-
+                    self.tooltip_handler.set_tooltip(variable.name, variable.name)
                 
         
     def onItemSelected( self, event ):
@@ -187,7 +230,7 @@ class ObjectTreeView(ttk.Treeview):
             self.cpu_list.append(cpu)
             parent = self.insert('', index = 'end', iid = cpu.name, text=cpu.objectName, 
                         image=self.image_storage['cpu'], values = values)
-            
+            self.tooltip_handler.set_tooltip(cpu.name, cpu.name)
             allObjects = cpu.externalObjects
             # read task names
             taskNames = [ _['name'] for _ in allObjects if _['type'] == 'Task'] 
@@ -196,7 +239,8 @@ class ObjectTreeView(ttk.Treeview):
                 values = task.status['ST']
                 self.insert( parent, index = 'end', iid = task.name, text = name,
                             image = self.image_storage['task'], values = values )    
-            
+                self.tooltip_handler.set_tooltip(task.name, task.name)
+    
         
     def insert_cpu(self, device : Device, ip : IPv4Address ):
         name = ip.compressed.replace('.','_')
